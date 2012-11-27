cc37f95c371115ebd72b3a181b2414c0fec0a5b3
Adam Prescott <prescott.adam@gmail.com>
2012-11-27 10:51:33 -0500

    Initial

---
+++ samples.ZipArchives/ExtractFileWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace samples.ZipArchives
+{
+    /// <summary>
+    /// Interaction logic for ExtractFileWindow.xaml
+    /// </summary>
+    public partial class ExtractFileWindow : Window
+    {
+        public ZipArchiveEntry Result { get; set; }
+
+        public ExtractFileWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            if (listBox.SelectedItem != null)
+            {
+                Result = listBox.SelectedItem as ZipArchiveEntry;
+                Close();
+            }
+        }
+    }
+}
---
+++ samples.ZipArchives/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace samples.ZipArchives
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static string PromptForDirectory()
+        {
+            var dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return dlg.SelectedPath;
+            }
+            return string.Empty;
+        }
+
+        private static string PromptForSaveFile(string file, string ext, string filter)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.FileName = file;
+            dlg.DefaultExt = ext;
+            dlg.Filter = filter;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return dlg.FileName;
+            }
+            return string.Empty;
+        }
+
+        private static string PromptForOpenFile(string file, string ext, string filter)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.FileName = file;
+            dlg.DefaultExt = ext;
+            dlg.Filter = filter;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return dlg.FileName;
+            }
+            return string.Empty;
+        }
+
+        private static ZipArchiveEntry PromptForArchiveEntry(ZipArchive zipArchive)
+        {
+            var dlg = new ExtractFileWindow();
+            dlg.DataContext = zipArchive.Entries;
+            dlg.ShowDialog();
+            return dlg.Result;
+        }
+
+        private void OnExtractArchive(object sender, RoutedEventArgs e)
+        {
+            var archive = PromptForOpenFile(
+                string.Empty, ".zip", "Zip archives (.zip)|*.zip");
+            if (string.IsNullOrEmpty(archive))
+                return;
+
+            var destination = PromptForDirectory();
+
+            ZipFile.ExtractToDirectory(archive, destination);
+        }
+
+        private void OnExtractFile(object sender, RoutedEventArgs e)
+        {
+            var archive = PromptForOpenFile(
+                string.Empty, ".zip", "Zip archives (.zip)|*.zip");
+            if (string.IsNullOrEmpty(archive))
+                return;
+
+            using (ZipArchive zipArchive = ZipFile.Open(archive, ZipArchiveMode.Read))
+            {
+                var itemToExtract = PromptForArchiveEntry(zipArchive);
+                if (itemToExtract == null)
+                    return;
+
+                var target = PromptForSaveFile(
+                    itemToExtract.FullName, string.Empty, "All files (.*)|*.*");
+
+                using (var fs = new FileStream(target, FileMode.Create))
+                {
+                    using (var contents = itemToExtract.Open())
+                    {
+                        contents.CopyToAsync(fs);
+                    }
+                }
+            }
+        }
+
+        private void OnCreateArchive(object sender, RoutedEventArgs e)
+        {
+            var dir = PromptForDirectory();
+            var target = PromptForSaveFile(
+                "Archive.zip", ".zip", "Zip archives (.zip)|*.zip");
+            ZipFile.CreateFromDirectory(dir, target);
+        }
+
+        private void OnAddFileToArchive(object sender, RoutedEventArgs e)
+        {
+            var archive = PromptForOpenFile(
+                string.Empty, ".zip", "Zip archives (.zip)|*.zip");
+            if (string.IsNullOrEmpty(archive))
+                return;
+
+            var file = PromptForOpenFile(
+                string.Empty, ".*", "All files (.*)|*.*");
+            if (string.IsNullOrEmpty(archive))
+                return;
+
+            using (ZipArchive zipArchive = ZipFile.Open(archive, ZipArchiveMode.Update))
+            {
+                var name = Path.GetFileName(file);
+                zipArchive.CreateEntryFromFile(file, name);
+            }
+        }
+    }
+}
